e489ecdb7aed83eecc198e67e9f55b1b1c73b7a8
bgegeny <gegeny.benjamin@gmail.com>
2022-09-01 23:31:37 +0200

    Init
    Initializing Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        NN Weather App
+      </header>
+    </div>
+  );
+}
+
+export default App;
