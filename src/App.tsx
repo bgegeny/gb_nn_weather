36506f54bcc69131b553b25d3b3b7a8820a3eaad
bgegeny <gegeny.benjamin@gmail.com>
2022-09-02 00:33:35 +0200

    installing libaries, PoC geolocation usage
    - installing bootstrap and sass
    - trying out geolocation Javascript API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
 
 function App() {
+
+    const [ latitude, setLatitude ] = useState(0)
+    const [ longitude, setLongitude ] = useState(0)
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            setLongitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+        });
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
